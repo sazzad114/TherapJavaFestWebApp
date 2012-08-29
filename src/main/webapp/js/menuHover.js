cf0e0d763baaa43b269e8db0d83d3222c2ca2e42
Md. Sazzadur Rahaman <sazzad114@gmail.com>
2012-08-29 16:50:36 +0600

    ui design has been changed

--- src/main/webapp/js/menuHover.js
+++ src/main/webapp/js/menuHover.js
@@ -9,23 +9,20 @@ function changeMenuBackground() {
 
     var path = window.location.pathname;
 
-    var menu = document.getElementById("menu");
+    var menu = document.getElementsByClassName("menu")[0];
     var a_elements = menu.getElementsByTagName("a");
 
-    //alert("entered here");
-    var alertString = path+" ";
 
     for (var i = 0, len = a_elements.length; len > i; i++) {
 
-        alertString += a_elements[i].pathname +" ";
-        if ((path.search(a_elements[i].pathname) >= 0 || (path.search("greetings") >=0 && a_elements[i].pathname.search("greetings") >= 0)) && a_elements[i].pathname != "/") {
+        if ((path.search(a_elements[i].pathname) >= 0 || (path.search("greetings") >= 0 && a_elements[i].pathname.search("greetings") >= 0)) && a_elements[i].pathname != "/") {
 
             a_elements[i].style.backgroundColor = '#f2622d';
         }
         else {
 
-            a_elements[i].style.backgroundColor = '#0086df';
+            a_elements[i].style.backgroundColor = '#0072b8';
         }
     }
-    //alert(alertString);
+
 }
